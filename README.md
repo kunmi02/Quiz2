c4766b20971c9ef282d9ed01eb63b581fc38caed
kunmi02 <mrkhunmie@gmail.com>
2019-01-23 11:05:14 +0100

    javascript work added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Quiz2
+This repo contains my javascript work with dufuna
---
+++ js/Script.js
@@ -0,0 +1,14 @@
+var person = {
+    name: ['Ibrahim Akinkunmi', 'Azeez'],
+    age: 21,
+    gender: "male",
+    interests: ['Coding', 'Creative thinking'],
+    bio: function() {
+    alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age +
+    ' years old. He likes ' + this.interests[0] + ' and ' +
+    this.interests[1] + '.');
+    },
+    greeting: function() {
+    alert('Hi! I\'m ' + this.name[0] + '.');
+    }
+    };
